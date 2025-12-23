f655ee29c3910ed1921b67da0729feadecae1593
wisdom oladipupo <wisbel71@gmail.com>
2025-12-23 22:14:57 +0100

    Initial Django project setup

---
+++ library/models.py
@@ -0,0 +1,7 @@
+from django import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    year = models.IntegerField()
+    pages = models.IntegerField()
---
+++ mysite/settings.py
@@ -0,0 +1,5 @@
+INSTALLED_APPS = [
+    'library',
+    'django.contrib.admin',
+    'django.contrib.auth',
+]
