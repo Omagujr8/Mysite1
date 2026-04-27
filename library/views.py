962dc2c32c7a09647f9452cc770ce5518e95e6e9
Omagu Emmanuel <omagujr8@gmail.com>
2026-04-27 20:07:09 +0100

    Feat: added login  on view.py

--- library/views.py
+++ library/views.py
@@ -45,3 +45,19 @@ def register(request):
         return redirect('dashboard')
 
     return render(request, 'library/auth/register.html')
+
+#login view
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, 'library/auth/login.html')
