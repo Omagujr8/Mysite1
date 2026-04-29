af74a4fe7a0fd11b083342fd1551a0f2ed052b41
Omagu Emmanuel <omagujr8@gmail.com>
2026-04-29 12:39:27 +0100

    feat(api/serializer & api/views): created serializers and api views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+class BookAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
