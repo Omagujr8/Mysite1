853190c36a13a25a1c34eb639d1a8b8e4239540b
Omagu Emmanuel <omagujr8@gmail.com>
2026-05-01 21:03:40 +0100

    Feat: Added Register URL

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookAPIView, book_detail
+from .views import BookAPIView, book_detail, register
 
 urlpatterns = [
     path('books/', BookAPIView.as_view(), name='book-list'),
     path('books/<int:pk>/', book_detail),
+    path('register/', register),
 ]
